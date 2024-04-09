247c01ac2d42f17d50b072d2c04be144892cf327
Colin McCaffery <mccaf2cm@dukes.jmu.edu>
2024-04-09 16:54:57 -0400

    this launches but I don't think it recognizes terraBlender in game

---
+++ common/src/main/java/net/world/biome/ModTerrablenderAPI.java
@@ -0,0 +1,9 @@
+package net.world.biome;
+
+import terrablender.api.TerraBlenderApi;
+
+public class ModTerrablenderAPI implements TerraBlenderApi {
+
+}
+
+// for some reason it doesn't recognize terrablenderapi
